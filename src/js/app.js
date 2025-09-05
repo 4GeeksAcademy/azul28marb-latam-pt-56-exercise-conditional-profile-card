5be84aee27e50f4c068b111e99f722382cca6a25
Azul MA <azulma2826@gmail.com>
2025-09-05 04:53:14 +0000

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -42,7 +42,11 @@ function render(variables = {}) {
           ${variables.city !== null ? variables.city : "City"}
           ${variables.country !== null ? variables.country : "Country"}
           </h3>
-          <ul class="position-right">
+          <ul class="position-right" ${
+            variables.socialMediaPosition !== null
+              ? variables.socialMediaPosition
+              : "Left"
+          }>
             <li><a href="https://twitter.com/${
               variables.twitter !== null ? variables.twitter : "Twitter"
             }"><i class="fab fa-twitter"></i></a></li>
